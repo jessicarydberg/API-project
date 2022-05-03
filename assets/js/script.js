15186fe790ecb88f4fd9c13fc26e2a0d4970e6c3
jessicarydberg <jessica.rydberg@hotmail.se>
2022-05-03 08:42:15 +0000

    Add post data

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,10 +19,35 @@ async function postForm (e) {
     const data = await response.json()
 
     if (response.ok) {
-        displayStatus(data);
+        displayErrors(data);
     } else {
         throw new Error(data.error);
     }
+}
+
+function displayErrors(data) {
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class"error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `
+            results += `column <span class="column">${error.col}</span>, </div>`
+            results += `<div class="error">${error.error}</div>`
+
+        }
+    }
+
+    let modalHeader = document.getElementById("resultsModalTitle")
+    let modalBody = document.getElementById("results-content")
+    
+    modalHeader.innerHTML = heading
+    modalBody.innerHTML = results
+    
+    resultsModal.show()
+
 }
 
 async function getStatus(e) {
